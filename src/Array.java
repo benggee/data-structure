a36c1e7a3dd7c6771d0d30964a82248a3fa71cf0
heads <seepre@seepre.com>
2019-06-19 10:27:45 +0800

    数据结构-数组

--- src/Array.java
+++ src/Array.java
@@ -30,18 +30,13 @@ public class Array<E> {
 
     // 在index索引的位置插入一个新元素e
     public void add(int index, E e){
-
         if(index < 0 || index > size)
             throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size.");
-
         if(size == data.length)
             resize(2 * data.length);
-
         for(int i = size - 1; i >= index ; i --)
             data[i + 1] = data[i];
-
         data[index] = e;
-
         size ++;
     }
 
@@ -91,13 +86,11 @@ public class Array<E> {
     public E remove(int index){
         if(index < 0 || index >= size)
             throw new IllegalArgumentException("Remove failed. Index is illegal.");
-
         E ret = data[index];
         for(int i = index + 1 ; i < size ; i ++)
             data[i - 1] = data[i];
         size --;
         data[size] = null; // loitering objects != memory leak
-
         if(size == data.length / 4 && data.length / 2 != 0)
             resize(data.length / 2);
         return ret;
@@ -122,7 +115,6 @@ public class Array<E> {
 
     @Override
     public String toString(){
-
         StringBuilder res = new StringBuilder();
         res.append(String.format("Array: size = %d , capacity = %d\n", size, data.length));
         res.append('[');
@@ -137,7 +129,6 @@ public class Array<E> {
 
     // 将数组空间的容量变成newCapacity大小
     private void resize(int newCapacity){
-
         E[] newData = (E[])new Object[newCapacity];
         for(int i = 0 ; i < size ; i ++)
             newData[i] = data[i];
